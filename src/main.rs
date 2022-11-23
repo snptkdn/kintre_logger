ea3443244198b67e380a3b7e967dfecad5c67864
snptkdn <400m.wflick@gmail.com>
2022-11-23 22:32:20 +0900

    起動時のハンドラを実装した。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+mod commands;
+
+use std::{collections::HashSet, fs::File, io::BufReader, usize};
+
+use serenity::async_trait;
+use serenity::framework::standard::{
+    help_commands,
+    macros::{group, help},
+    Args, CommandGroup, CommandResult, HelpOptions,
+};
+use serenity::framework::StandardFramework;
+use serenity::model::{channel::Message, gateway::Ready, id::UserId};
+use serenity::prelude::{Client, Context, EventHandler};
+
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+use commands::{channels::*, neko::*};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
 }
